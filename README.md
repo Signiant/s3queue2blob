95bd73f4d7f2f3e99b1ed6ca1c2e6fff6af021ea
Mike Perttula <mperttula@signiant.com>
2017-06-30 14:32:19 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# s3queue2blob
+
+Polls an SQS queue for S3 Put/Post/Copy events on a publicly available bucket and replicates to Azure blob storage with an azure pull.
+
+### Requirements
+- You must have an s3 bucket that allows read to everyone.
+- You must setup your bucket to send PUT, Post, Copy and Complete Multipart Upload events to an SQS Queue
+- Your credentials profile must have access to poll the queue.
+- Your queue should have a policy to allow the bucket to send messages to it.
+
+### Example SQS Permission:
+```
+{
+  "Version": "2012-10-17",
+  "Id": "addmessage",
+  "Statement": [
+    {
+      "Sid": "sqsAllow",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": "SQS:SendMessage",
+      "Resource": "arn:aws:sqs:us-east-1:ACCOUNT_NUMBER:QUEUE_NAME",
+      "Condition": {
+        "ArnLike": {
+          "aws:SourceArn": "arn:aws:s3:*:*:NAME_OF_BUCKET"
+        }
+      }
+    }
+  ]
+}
+```
+Where ACCOUNT_NUMBER is you account ID, QUEUE_NAME is the name of your Queue, and NAME_OF_BUCKET is the bucket that is sending the s3 events.
+
+
+### Sample config file format
+```
+{
+  "QUEUE":"QUEUE",
+  "REGION":"AWS REGION IF DEPLOYED IN AWS",
+  "S3REGION":"S3 REGION",
+  "STORAGE_ACCOUNT":"AZURE STORAGE ACCOUNT",
+  "CONTAINER":"AZURE CONTAINER",
+  "PROFILE":"AWS CREDENTIAL PROFILE"
+}
+```
+
+Sample docker run command showing how to mount the config file into the container using a data volume on the local docker host :
+```
+docker run -d --privileged=true \
+              -v /my/local/folder/config.json:/usr/src/app/config.json \
+              --name s3queue2blob \
+              signiant/s3queue2blob
+```
---
+++ queue2blob.py
@@ -0,0 +1,207 @@
+import argparse
+import os, sys
+import urllib2
+import boto3
+import json
+import time
+import logging
+from azure.storage.blob import BlockBlobService
+
+WAIT_TIME=60
+blob_service = ""
+debug = False
+
+#setup logging
+FORMAT = '%(asctime)-15s [%(levelname)s] %(message)s'
+logging.basicConfig(format=FORMAT, stream=sys.stdout, level=logging.INFO)
+logging.getLogger("botocore").setLevel(logging.CRITICAL)
+logging.getLogger("boto3").setLevel(logging.CRITICAL)
+logger = logging.getLogger(__name__)
+
+
+def parse_config_file(config_file):
+    global logger
+    if not os.path.exists(config_file):
+        s3_client = boto3.client('s3')
+        local_path = "/tmp/config.json"
+        s3bucket,s3config = config_file.split('/')
+        s3_client.download_file(s3bucket, s3config, local_path)
+        logger.info('Loaded configuration from S3')
+        profile="None"
+    else:
+        local_path = config_file
+        print ("LOCAL PATH: " + local_path)
+        profile="dev2"
+        logger.info('Loaded configuration from local file system')
+    with open(local_path) as stream:
+        config = {}
+        try:
+            config = json.load(stream)
+        except ValueError, e:
+            print(e)
+            sys.exit(1)
+
+        queue = config['QUEUE']
+        region = config['REGION']
+        s3_region = config['S3REGION']
+        storage_account = config['STORAGE_ACCOUNT']
+        container = config['CONTAINER']
+        logger.debug('Values : %s,%s,%s,%s',queue,region,storage_account,container)
+        return queue, region, s3_region, profile, storage_account, container
+
+
+def init_blob_service(storage_account,region,profile,key):
+    global blob_service
+    global logger
+    storage_key = "None"
+    #get key
+        storage_key = key
+
+    try:
+        blob_service = BlockBlobService(account_name=storage_account, account_key=storage_key)
+    except Exception as e:
+        logger.error("ERROR: Could connect to Azure Storage: %s", str(e))
+        return False
+    return True
+def upload_to_azure(source_files,container_name):
+    global blob_service
+    global logger
+    for fileobject in source_files:
+        logger.info('Uploading Fileobject: %s Sourceurl: %s',fileobject,source_files[fileobject])
+        try:
+            copy = blob_service.copy_blob(container_name, fileobject, source_files[fileobject])
+        except Exception as e:
+            #does not exist
+            logger.error('Could not upload to container: %s',str(e))
+            return False
+        count = 0
+        copy = blob_service.get_blob_properties(container_name, fileobject).properties.copy
+        while copy.status != 'success':
+            count = count + 1
+            if count > 50:
+                print('ERROR: Timed out waiting for async copy to complete.')
+                return False
+            logger.info('Copying to blob storage: %s',fileobject)
+            time.sleep(5)
+            copy = blob_service.get_blob_properties(container_name, fileobject).properties.copy
+        logger.info('Upload complete: %s',fileobject)
+        return True
+
+def poll_queue(queue_name, region, s3_region, profile, container):
+    global debug
+    global logger
+    try:
+        if profile != "None":
+            session = boto3.session.Session(profile_name=profile, region_name=region)
+        else:
+            session = boto3.session.Session(region_name=region)
+        sqs = session.resource('sqs')
+    except Exception as e:
+        logger.error("Could not connect to AWS: %s",str(e))
+        return False
+
+    try:
+        queue = sqs.get_queue_by_name(QueueName=queue_name)
+    except Exception as e:
+        logger.error("Could not load queue %s: %s",queue_name,str(e))
+        return False
+    # Process messages
+    max_queue_messages = 10
+    message_bodies = []
+    success = True
+    while True:
+        messages_to_delete = []
+        for message in queue.receive_messages(MaxNumberOfMessages=max_queue_messages):
+            # process message body
+            try:
+                body = json.loads(message.body)
+            except Exception as e:
+                logger.error("Could not load messaged body: %s",str(e))
+                return False
+            to_upload = {}
+            if len(body) > 0:
+                logger.debug('Message found')
+                try:
+                    logger.debug('Region: %s Bucket: %s File: %s ',body['Records'][0]['awsRegion'],body['Records'][0]['s3']['bucket']['name'],body['Records'][0]['s3']['object']['key'])
+                    file_object = body['Records'][0]['s3']['object']['key']
+                except KeyError, e:
+                    logger.info('Found non s3 upload message, removing')
+                    messages_to_delete.append({
+                        'Id': message.message_id,
+                        'ReceiptHandle': message.receipt_handle
+                    })
+                except IndexError, e:
+                    logger.error('ERROR: IndexError received: %s',str(e))
+                    return False
+                else:
+                    message_url = "https://" + s3_region + "/"+ body['Records'][0]['s3']['bucket']['name'] + "/" + file_object
+                    to_upload.update({file_object:message_url})
+                    upload_result = upload_to_azure(to_upload,container)
+                    # add message to delete
+                    if upload_result:
+                        messages_to_delete.append({
+                            'Id': message.message_id,
+                            'ReceiptHandle': message.receipt_handle
+                        })
+                    else:
+                        #exception occured on upload
+                        success = False
+        if len(messages_to_delete) == 0:
+            break
+        # delete messages to remove them from SQS queue
+        # handle any errors
+        else:
+            try:
+                delete_response = queue.delete_messages(Entries=messages_to_delete)
+            except Exception as e:
+                logger.error("Could not delete messages from queue: %s",str(e))
+                return False
+    return success
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='queue2blob.py')
+
+    parser.add_argument("--config-file", help="config file containing required config", dest="config_file")
+    parser.add_argument("--queue", help="Queue name", dest="queue_name")
+    parser.add_argument("--region", help="AWS Region that the Queue is in", dest="region")
+    parser.add_argument("--s3region", help="The region prefix for s3 downloads", dest="s3_region")
+    parser.add_argument("--profile", help="The name of an aws cli profile to use.", dest='profile', required=False)
+    parser.add_argument("--storage", help="The name of storage account to use.", dest='storage_account', required=False)
+    parser.add_argument("--key", help="The key for the storage account", dest='storage_key', required=False)
+    parser.add_argument("--container", help="The container for the blob.", dest='container', required=False)
+    parser.add_argument("--debug", help="Set debug flag", action='store_true', dest='debug', required=False)
+    args = parser.parse_args()
+
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+    if args.config_file:
+        queue_name, region, s3_region, profile, storage_account, container  = parse_config_file(args.config_file)
+    elif os.environ.get('CONFIG_FILE') != 'None':
+        queue_name, region, s3_region, profile, storage_account, container  = parse_config_file(os.environ.get('CONFIG_FILE'))
+    else:
+        queue_name = args.queue_name
+        region = args.region
+        profile = args.profile
+        storage_account = args.storage_account
+        s3_region = args.s3_region
+        container= args.container
+    if args.storage_key:
+        key = args.storage_key
+    else:
+        key = "None"
+
+    if not init_blob_service(storage_account,region,profile,key):
+        sys.exit(1)
+    while True:
+        logger.info('Starting run')
+        result = poll_queue(queue_name, region, s3_region, profile, container)
+        if result:
+            logger.info('Completed run')
+            if os.environ.get('CONTAINER_NAME') != 'None':
+                if push_success_metrics() != 0:
+                    logger.error("Could not push success to Cloudwatch")
+        else:
+            logger.info('No messages processed , completed run')
+
+        time.sleep(WAIT_TIME)
